24154f30574bbbf0017f1a29d2809e7de8e46a72
Mohamed Rojbeni <rojbenimohamed@gmail.com>
2023-03-02 11:45:07 +0100

    dockerize footz

---
+++ footz-api/src/main/java/com/footz/Footz.java
@@ -0,0 +1,13 @@
+package com.footz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Footz {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Footz.class, args);
+	}
+
+}
